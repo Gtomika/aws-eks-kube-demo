5b541f0c1eecd51a526799b8a5c69e0bb79750a5
tamas-gaspar <tamas.gaspar@prt.manomano.com>
2023-09-04 20:50:24 +0200

    Fix eks aws auth config map

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,15 +56,22 @@ module "eks" {
   ]
 }
 
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
-  }
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+  depends_on = [
+    module.eks.eks_managed_node_groups,
+  ]
 }
 
+data "aws_eks_cluster_auth" "clusterauth" {
+  name = module.eks.cluster_name
+  depends_on = [
+    module.eks.eks_managed_node_groups,
+  ]
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.clusterauth.token
+}
