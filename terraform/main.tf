430778987b91fa526839db040d9e9ca4ee44fb03
tamas-gaspar <tamas.gaspar@prt.manomano.com>
2023-09-04 20:09:36 +0200

    Upgrade kube version

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ module "eks" {
   version = "~> 19.0"
 
   cluster_name = var.cluster_name
-  cluster_version = "1.19"
+  cluster_version = "1.27"
   cluster_endpoint_public_access = true
   vpc_id = module.vpc.vpc_id
   subnet_ids = concat(module.vpc.private_subnet_ids, module.vpc.public_subnet_ids)
