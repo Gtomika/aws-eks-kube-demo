f62e3aade3ebc3b0e0ee3405eb925b60068100d2
tamas-gaspar <tamas.gaspar@prt.manomano.com>
2023-09-11 15:18:38 +0200

    Add AWS load balancer controller to the cluster.

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,12 +51,12 @@ module "eks" {
 }
 
 # adds the AWS load balancer controller to the cluster
-module "aws_eks_load_balancer_controller" {
-  source = "lablabs/eks-load-balancer-controller/aws"
-  enabled = true
-  cluster_name                     = module.eks.cluster_name
-  cluster_identity_oidc_issuer     = module.eks.oidc_provider
+module "load_balancer_controller" {
+  source = "git::https://github.com/DNXLabs/terraform-aws-eks-lb-controller.git"
+
+  cluster_identity_oidc_issuer     = module.eks.cluster_oidc_issuer_url
   cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
+  cluster_name                     = module.eks.cluster_id
 }
 
 data "aws_eks_cluster" "cluster" {
