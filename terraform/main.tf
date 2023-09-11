41bd1ce433c65d37d6d64c89a7098728a91c9019
tamas-gaspar <tamas.gaspar@prt.manomano.com>
2023-09-11 15:24:40 +0200

    Remove AWS load balancer controller to the cluster.

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,15 +50,6 @@ module "eks" {
   }]
 }
 
-# adds the AWS load balancer controller to the cluster
-module "load_balancer_controller" {
-  source = "git::https://github.com/DNXLabs/terraform-aws-eks-lb-controller.git"
-
-  cluster_identity_oidc_issuer     = module.eks.cluster_oidc_issuer_url
-  cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
-  cluster_name                     = module.eks.cluster_id
-}
-
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
   depends_on = [
